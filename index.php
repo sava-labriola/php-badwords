3132015384eb1817c3017213fcd665067e58e497
sava-labriola <labriolasava00@gmail.com>
2020-06-16 09:48:35 +0200

    esercizio completo

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<!-- predisponete una variabile contenente un breve testo, poi tramite le funzioni sulle stringhe di php censurate
+una "badword" che viene passata in GET con *** (3 simboli di asterico).
+Stampate a schermo il testo censurato all'interno di un paragrafo e la lunghezza del paragrafo stesso. -->
+
 <?php
     $frase = 'Oggi sta piovendo molto';
-    $parola_censurata = $_GET = ['badword'];
+    $parola_censurata = $_GET['badword'];
     $censura = '***';
     $testo_censurato = str_replace($parola_censurata, $censura, $frase);
-    $lunghezza_frase = strlen($testo_censurato);
+    $lunghezza_frase = strlen($frase);
 ?>
 
 
@@ -16,7 +20,9 @@
     </head>
     <body>
         <section>
+            <h1>Testo censurato</h1>
             <p> <?php echo $testo_censurato ?></p>
+            <h2>Lunghezza frase</h2>
             <p> <?php echo $lunghezza_frase ?></p>
         </section>
     </body>
