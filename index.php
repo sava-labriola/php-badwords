5507a9c13128fb80bd426c45a273f7eeeb408d9a
sava-labriola <labriolasava00@gmail.com>
2020-06-16 12:40:13 +0200

    esercizio completo fix

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<!-- predisponete una variabile contenente un breve testo, poi tramite le funzioni sulle stringhe di php censurate
-una "badword" che viene passata in GET con *** (3 simboli di asterico).
-Stampate a schermo il testo censurato all'interno di un paragrafo e la lunghezza del paragrafo stesso. -->
-
 <?php
+// predisponete una variabile contenente un breve testo, poi tramite le funzioni sulle stringhe di php censurate
+// una "badword" che viene passata in GET con *** (3 simboli di asterico).
+// Stampate a schermo il testo censurato all'interno di un paragrafo e la lunghezza del paragrafo stesso.
+
+
     $frase = 'Oggi sta piovendo molto';
     $parola_censurata = $_GET['badword'];
     $censura = '***';
